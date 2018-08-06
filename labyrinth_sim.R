823d6789bfe9ff3cf174936c0832686a15ba0157
Jason Vander Woude <jasonvwoude@gmail.com>
2018-08-06 15:16:11 -0500

    Update LabyrinthMask

--- labyrinth_sim.R
+++ labyrinth_sim.R
@@ -38,84 +38,120 @@ require(digest, quietly=T)
 
 
 
-mimic <- function(vcf, full.out, sample.out, parents, n.gen, read.err,
-                  parental.results=NULL, use.50=FALSE, parallel=TRUE, cores=4) {
+LabyrinthMask <- function(vcf, parents, depth=0, lik.ratio=100, rerr=0.01) {
 
-    total.timer <- new.timer()
-    print.labyrinth.sim.header()
+    get.liklihood.ratio <- function(depth.a, depth.b, rerr) {
+        if (length(depth.a) > 1)
+            stop("length of depth.a must be 1")
+        if (length(depth.b) > 1)
+            stop("length of depth.b must be 1")
 
-    ## vcf load code
-    if (! inherits(vcf, "vcfR")) {
-        timer <- new.timer()
-        display(0, "Loading vcf")
-        vcf <- read.vcfR(vcf, verbose=F)
-        display(1, "Completed in ", timer(), "\n")
+        pa <- (1-rerr)^depth.a * (rerr)^depth.b
+        pb <- (rerr)^depth.a * (1-rerr)^depth.b
+        ph <- (0.5)^depth.a * (0.5)^depth.b
+
+        if (depth.a == 0) {
+            if (depth.b == 0)
+                0.5
+            else
+                pb / (pa + ph)
+        } else if (depth.b == 0) {
+            pa / (pb + ph)
+        } else {
+            ph / (pa + pb)
+        }
     }
 
-    display(5, "BIALLELIC")
 
+    ad.arr <- get.ad.array(vcf)
+    depths <- apply(ad.arr, 1:2, sum)  # sum individual allelic reads
+    liklihood.ratios <- apply(ad.arr, 1:2, function(reads) {
+        get.liklihood.ratio(reads[1], reads[2], rerr)
+    })
+
+    parental         <- matrix(rep(getSAMPLES(vcf) %in% parents, nrow(ad.arr)),
+                               nrow=nrow(ad.arr),
+                               byrow=TRUE)
+    sufficient.depth <- depths >= depth
+    sufficient.ratio <- liklihood.ratios >= lik.ratio
+    mask <- sufficient.depth & sufficient.ratio & !parental
+    print(paste0("Masking ", sum(mask), " sites"))
+
+    new.ad <- getAD(vcf)
+    new.ad[mask] <- "0,0"
+
+    new.gt <- getGT(vcf)
+    new.gt[mask] <- "./."
+
+    new.dp <- depths
+    new.dp[mask] <- "0"
+
+
+    concat <- matrix(paste0(new.gt, ":", new.ad, ":", new.dp), nrow=nrow(new.ad))
+    vcf@gt <- cbind("GT:AD:DP", concat)
+
+    colnames(vcf@gt) <- c("FORMAT", colnames(new.ad))
+    rownames(vcf@gt) <- rownames(new.ad)
+
+    vcf  # implicit return
+}
+
+
+LabyrinthMimic <- function(parental, full.out, sample.out, parallel=TRUE, cores=4) {
+
+    total.timer <- new.timer()
+    print.labyrinth.header()
+
+
+    if (! verify.file.extension(full.out, ".vcf.gz")) {
+        display(0, "Output file (full.out) name must end with '.vcf.gz'\n")
+        stop()
+    }
+
+    if (! verify.file.extension(sample.out, ".vcf.gz")) {
+        display(0, "Output file (sample.out) name must end with '.vcf.gz'\n")
+        stop()
+    }
+
+
+    ## Load variables from parental
     timer <- new.timer()
-    display(0, "Generating necessary variables")
+    display(0, "Restoring variables from parental imputation result")
 
-    ad <- get.ad.array(vcf)
+    vcf <- parental$vcf
+    parents <- parental$parents
+    parent.models <- parental$parent.models
+    generation <- parental$generation
+    read.err <- parental$read.err
+    geno.err <- parental$geno.err
+    site.pair.transition.probs <- parental$site.pair.transition.probs
+    snp.chroms <- parental$snp.chroms
+    u.chroms <- parental$u.chroms
+    ad <- parental$ad
+    sample.names <- parental$sample.names
+    marker.names <- parental$marker.names
+    emission.structure <- parental$emission.structure
+
     depths <- ad[ , , 1] + ad[ , , 2]  # total read depth at each marker/member pair
-
-    snp.chroms <- getCHROM(vcf)
-    u.chroms <- unique(snp.chroms)
-    ## ad <- get.ad.array(vcf)
-    sample.names <- getSAMPLES(vcf)
     parent.indices <- which(sample.names %in% parents)
-    marker.names <- getID(vcf)
     n.mem <- length(sample.names)
+
     display(1, "Completed in ", timer(), "\n")
 
 
+    ## if (! use.50) {
+    ##     recomb.probs <- lapply(recomb.probs, function(chrom.recombs) {
+    ##         result <- chrom.recombs
+    ##         result[result > 0.49] <- 0.05
+    ##         result
+    ##     })
+    ## }
 
-    if (is.null(parental.results)) {
-        ## emission probability code
-        timer <- new.timer()
-        display(0, "Generating emission probabilities")
-        emission.structure <- get.emission.structures(ad,
-                                                      sample.names,
-                                                      marker.names,
-                                                      read.err)
-        display(1, "Completed in ", timer(), "\n")
-
-
-
-
-        ## parental imputation and recombination rate estimates
-        timer <- new.timer()
-        display(0, "Imputing parents and estimating recombination rates")
-        parental.results <- determine.parents.and.recombs(emission.structure,
-                                                          parents,
-                                                          snp.chroms,
-                                                          marker.names,
-                                                          sample.names,
-                                                          parallel,
-                                                          cores)
-        display(1, "Completed in ", timer(), "\n")
-    }
-
-
-
-    parental.models <- lapply(parental.results$parental.models,
-                              function(chrom.model){
-                                  parental.model.to.parents(chrom.model$path)
-                              })
-    recomb.probs <- parental.results$recombs
-    if (! use.50) {
-        recomb.probs <- lapply(recomb.probs, function(chrom.recombs) {
-            result <- chrom.recombs
-            result[result > 0.49] <- 0.05
-            result
-        })
-    }
-
-    gt <- do.call(rbind, lapply(seq_along(parental.models), function(i) {
-        p1 <- sequence.to.genome(parental.models[[i]][[1]])
-        p2 <- sequence.to.genome(parental.models[[i]][[2]])
-        f1 <- cross(p1, p2, recomb.probs[[i]])
+    gt <- do.call(rbind, lapply(seq_along(parent.models), function(i) {
+        indiv.parent.models <- extract.each.parent(parent.models$model)
+        p1 <- sequence.to.genome(indiv.parent.models[[1]])
+        p2 <- sequence.to.genome(indiv.parenta.models[[2]])
+        f1 <- cross(p1, p2, parent.models$recombs[[i]])
 
         mimic.chrom.common.f1(p1,
                               p2,
@@ -123,7 +159,7 @@ mimic <- function(vcf, full.out, sample.out, parents, n.gen, read.err,
                               recomb.probs[[i]],
                               parent.indices,
                               n.mem,
-                              n.gen)
+                              generation)
     }))
 
     cols <- colnames(vcf@gt)
@@ -178,39 +214,6 @@ mimic <- function(vcf, full.out, sample.out, parents, n.gen, read.err,
 
     sample.ad <- apply(sample.ad, 1:2, paste0, collapse=",")
 
-    ## ## TODO(Jason): This code is ad-hoc and could be cleaned
-    ## sample.ad <- matrix(mapply(function(depth, gt) {
-    ##     if (depth == 0)
-    ##         return("0,0")
-
-    ##     if (gt == "0/0")
-    ##         probs <- c(1-read.err, read.err)
-    ##     else if (gt == "0/1")
-    ##         probs <- c(0.5, 0.5)
-    ##     else if (gt == "1/1")
-    ##         probs <- c(read.err, 1-read.err)
-    ##     else
-    ##         stop("This should never happen. Error in gt type")
-
-    ##     reads <- sample(0:1, size=depth, replace=TRUE, prob=probs)
-    ##     n.alt <- sum(reads)
-    ##     paste0((depth - n.alt), ",", n.alt)
-    ## }, depths, gt), nrow=nrow(depths), ncol=ncol(depths))
-
-    ## sample.gt <- apply(sample.ad, 1:2, function(ad) {
-    ##     depths <- str.split(ad)
-    ##     if (all(depths=="0"))
-    ##         gt <- "./."
-    ##     else if (depths[1] == "0")
-    ##         gt <- "1/1"
-    ##     else if (depths[2] == "0")
-    ##         gt <- "0/0"
-    ##     else
-    ##         gt <- "0/1"
-
-    ##     gt  # implicit return
-    ## })
-
     concat <- matrix(paste0(sample.gt, ":", sample.ad, ":", depths), nrow=nrow(sample.gt))
     cols <- colnames(vcf@gt)
     rows <- rownames(vcf@gt)
@@ -223,7 +226,13 @@ mimic <- function(vcf, full.out, sample.out, parents, n.gen, read.err,
 }
 
 
-mimic.chrom.common.f1 <- function(p1, p2, f1, recomb.probs, parent.indices, n.mem, n.gen) {
+## When the parents are not totally homozygous for different alleles, then it
+## matters whether a single F1 plant is selfed to create the population of F2s
+## or if the parents are crossed multiple times to create a generation of
+## different F1 plants
+mimic.chrom.common.f1 <- function(p1, p2, f1, recomb.probs, parent.indices,
+                                  n.mem, n.gen) {
+
     gts <- lapply(seq_len(n.mem),
                   function(i) {
                       if (i == parent.indices[1]) {
@@ -345,7 +354,6 @@ generate.selfed.member.common.f1 <- function(f1, n.gen, recomb.probs) {
 }
 
 
-
 generate.selfed.member <- function(p1, p2, n.gen, recomb.probs) {
     member <- cross(p1, p2, recomb.probs)
     for (i in seq_len(n.gen - 1)) {
@@ -359,27 +367,3 @@ member.to.gt.str <- function(member) {
     gt <- mapply(sum, member$cA, member$cB)  # boolean addition will be 0, 1, or 2
     c("0/0", "0/1", "1/1")[gt + 1]
 }
-
-
-print.labyrinth.sim.header <- function() {
-
-    ## the image looks funny because '\' in the displayed image must be '\\' in the code
-    writeLines("")
-    writeLines(" _____________________________________________________________________")
-    writeLines("|          __          ____        ____  _____                        |")
-    writeLines("|         / /         / __ \\      / __ \\/_  _/                        |")
-    writeLines("|        / /   ____  / /_/ /_  __/ /_/ / / / __   __________  __      |")
-    writeLines("|       / /   / _  \\/ _  _/\\ \\/ / _  _/ / / /  | / /_  __/ /_/ /      |")
-    writeLines("|      / /___/ /_/ / /_\\ \\  \\  / / \\ \\_/ /_/ /||/ / / / / __  /       |")
-    writeLines("|     /_____/_/ /_/______/  /_/_/  /_/____/_/ |__/ /_/ /_/ /_/        |")
-    writeLines("|                                                                     |")
-    writeLines("|     ========  P O P U L A T I O N   S I M U L A T O R  ========     |")
-    writeLines("|_____________________________________________________________________|")
-    writeLines("")
-}
-
-
-
-
-
-
